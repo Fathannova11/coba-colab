20f44820368f13bd3adb60f8ed6d247a1de67e1f
fathanNova <novaaroyan13@gmail.com>
2025-05-28 11:05:47 +0700

    tambah teks

--- Struktur Data/tesColab.cpp
+++ Struktur Data/tesColab.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <conio.h>
 using namespace std;
 
 int main(){
-    
+    system("cls");
+
+    cout<<"\nSaat ini saya sedang mencoba untuk bekerja bersama di repository milik Fathannova11\n";
+    getch();
 }
